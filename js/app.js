507e62c6d5369ba0574533a02396f1b754f9dcb8
Mark Yackley <myackley@hotmail.com>
2014-11-30 22:57:13 -0600

    Set up: form sunmit handler, New Game click handler Initialized: base variables, random number function, new game and supporting functions, guess checking

--- js/app.js
+++ js/app.js
@@ -1,16 +1,76 @@
 
 $(document).ready(function(){
-	
+
+	var generatedNum;
+	var guessCnt;
+	var pastguesses;
+
+	var generate = function() {
+		generatedNum = Math.floor((Math.random()*100)+1);
+		console.log("The number: "+generatedNum);
+		return generatedNum;
+	}
+	var newGame = function() {
+		generate();
+		clearUI();
+	}
+	var clearUI = function() {
+		// |-- reset variables
+		var guessCnt = $("#count").text("0");
+		// |-- clear ui
+		$("#guessList li").remove();
+		console.log("cleared");
+	}
+
+	var checkGuesses = function(pnum,unum) {
+		if (pnum.val() == unum) { // already guessed
+			return false; // matches
+		} else {
+			return true; // good to go
+		}
+	};
+
+	var displayGuess = function(num) {
+		$("#guessList").append("<li>" + num + "</li>");
+	};
+
+	$("form").on("submit", function(event) {
+		event.preventDefault();
+		//init variables
+		userguess = $("#userGuess").val(); //guess
+		guessCnt = $("#count").text(); // guess counter
+		pastguesses = $("#guessList").children("li");
+
+		//	see if guessed alread or no guesses exist
+		checkGuesses(pastguesses,userguess);
+
+		// |-- validate guess
+		//      |-- already guessed? => checkGuesses==false
+
+		//      |-- string?
+		//      |-- decimal?
+
+		// |-- if/else for cold, warm, warmer, hot
+
+		// push to html
+		displayGuess(userguess);
+		
+	});
+
+	$(".new").on("click", newGame());
+
+
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
 });
 
