30a24a9576da2d245a254e0778fdffa43d907f90
Mark Yackley <myackley@hotmail.com>
2014-12-01 20:57:26 -0600

    Refactored JS to simplify code

--- js/app.js
+++ js/app.js
@@ -1,64 +1,60 @@
 
 $(document).ready(function(){
 
-	var generatedNum;
-	var guessCnt;
-	var pastguesses;
-
 	var generate = function() {
 		generatedNum = Math.floor((Math.random()*100)+1);
 		console.log("The number: "+generatedNum);
 		return generatedNum;
-	}
-	var newGame = function() {
-		generate(); // generate number
-		clearUI(); // clear counter and listed guesses
-	}
-	var clearUI = function() {
-		$("#count").text("0"); // reset counter
-		$("#guessList li").remove(); // clear ui
+	};
+
+	var resetUI = function() {
+		$("#guessList").empty();
+		$("#feedback").text("Make your guess!");
 		console.log("cleared");
-	}
-
-	var checkGuesses = function(pnum,unum) {
-		if (pnum.val() == unum) { // already guessed
-			return false; // matches
-		} else {
-			return true; // good to go
-		}
 	};
 
-	var displayGuess = function(num) {
-		$("#guessList").append("<li>" + num + "</li>");
+	var newGame = function() {
+		resetUI();
+		generatedNum = generate();
 	};
 
-	$("form").on("submit", function(event) {
+	var displayGuess = function() {
+		$("#guessList").append("<li>" + userguess + "</li>");
+		return userguess;
+	};
+
+	var generatedNum = generate();
+	var guessCnt = 0 // guess counter
+
+	$(".button").on("click", function(event) {
 		event.preventDefault();
-		//init variables
+
+		guessCnt++;
+
+		// store guess
 		userguess = $("#userGuess").val(); //guess
-		pastguesses = $("#guessList").children("li");
-		guessCnt = $("#guessList").children("li").length+1; // guess counter
 
-		//	see if guessed alread or no guesses exist
-		checkGuesses(pastguesses,userguess);
 
-		// |-- validate guess
-		if (checkGuesses() == false) {
-			$("#feedback").text("You entered that number previously"); // already guessed?
-		} else if (userguess%1 != 0) {
+		// validate guess
+		if (userguess%1 != 0) {
 			$("#feedback").text("You are not allowed to enter decimals"); // decimal?
 		} else if ($.type(userguess) === "string") {
 			$("#feedback").text("You are not allowed to enter text"); // string?
 		} else if (userguess < 1 || userguess > 100) {
 			$("#feedback").text("Please enter a number between 1 and 100"); // between 1 and 100?
 		} else {
-			return false;
+			// cold, warm, warmer, hot
+			// if () {
+
+			// } else {
+
+			// }
 		}
 
-		// |-- if/else for cold, warm, warmer, hot
+		// display guess
+		displayGuess();
 
 		// push to html
-		displayGuess(userguess); // ... to guess list
 		$("#count").text(guessCnt); // ... to counter
 		
 	});
