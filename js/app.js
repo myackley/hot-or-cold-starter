76bce90fad26728f2ff9e4dda6446e20d566edfe
Mark Yackley <myackley@hotmail.com>
2014-12-01 21:55:48 -0600

    Completed validation for decimals, text entry, and proper range and completed cold, warm, warmer, and hot if/else statement

--- js/app.js
+++ js/app.js
@@ -3,34 +3,34 @@ $(document).ready(function(){
 
 	var generate = function() {
 		generatedNum = Math.floor((Math.random()*100)+1);
-		console.log("The number: "+generatedNum);
 		return generatedNum;
 	};
 
 	var resetUI = function() {
+		$("#feedback").text("Make your Guess!");
+		$("#userGuess").val("");
+		$("#count").text("0");
 		$("#guessList").empty();
-		$("#feedback").text("Make your guess!");
-		console.log("cleared");
-	};
-
-	var newGame = function() {
-		resetUI();
-		generatedNum = generate();
+		guessCnt = 0;
 	};
 
 	var displayGuess = function() {
 		$("#guessList").append("<li>" + userguess + "</li>");
-		return userguess;
+	};
+
+	var newGame = function() {
+		resetUI(); // clear out ui and reset counter
+		generatedNum = generate(); // generate new number
 	};
 
 	var generatedNum = generate();
-	var guessCnt = 0 // guess counter
+	var guessCnt = 0; // guess counter
 
 	$(".button").on("click", function(event) {
 		event.preventDefault();
 
+		// +1 to count
 		guessCnt++;
-
 		// store guess
 		userguess = parseInt($("#userGuess").val(),10); //guess
 
@@ -43,18 +43,28 @@ $(document).ready(function(){
 			$("#feedback").text("Please enter a number between 1 and 100"); // between 1 and 100?
 		} else {
 			// cold, warm, warmer, hot
-			// if () {
-
-			// } else {
-
-			// }
+			if (userguess == generatedNum) {
+				$("#feedback").text("Nailed it!");
+			} else if (Math.abs(generatedNum-userguess) < 3) {
+				$("#feedback").text(userguess + " is hot!");
+			} else if (Math.abs(generatedNum-userguess) < 9) {
+				$("#feedback").text(userguess + " is getting warmer!");
+			} else if (Math.abs(generatedNum-userguess) < 15) {
+				$("#feedback").text(userguess + " is cold.");
+			} else if (Math.abs(generatedNum-userguess) < 25) {
+				$("#feedback").text(userguess + " is really cold.");
+			} else if (Math.abs(generatedNum-userguess) < 35) {
+				$("#feedback").text(userguess + " is freezing cold.");
+			} else {
+				$("#feedback").text("Colder than winter in Antartica");
+			}
 		}
 
 		// display guess
 		displayGuess();
 
-		// push to html
-		$("#count").text(guessCnt); // ... to counter
+		// push count to html
+		$("#count").text(guessCnt);
 		
 	});
 
